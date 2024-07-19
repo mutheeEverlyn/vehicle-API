e4ccdf60b14cfbbc1e44943e95c77e297ff44aec
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-19 15:06:32 +0300

    changes

--- src/payments/payments.router.ts
+++ src/payments/payments.router.ts
@@ -5,23 +5,11 @@ import { paymentsSchema } from "../validators";
 import { adminRoleAuth,userRoleAuth,userAdminRoleAuth} from "../middleware/bearAuth";
 export const paymentsRouter = new Hono();
 
-paymentsRouter .get("/payments",adminRoleAuth, listPayments);
+paymentsRouter .get("/payments",userAdminRoleAuth, listPayments);
 paymentsRouter .get("/payments/:id",userAdminRoleAuth, getPayments)
-// paymentsRouter .post("/payments",zValidator('json',paymentsSchema,(result,c) =>{
-//     if(!result.success){
-//         return c.json(result.error,400)
-//     }
-// }),adminRoleAuth, createPayment)
 paymentsRouter .put("/payments/:id",adminRoleAuth,updatePayments)
 paymentsRouter .delete("/payments/:id",adminRoleAuth, deletePayments)
 paymentsRouter .get("/paymentsData",userAdminRoleAuth,getPaymentsData);
-// paymentsRouter.post('/create-payment-intent', userRoleAuth, createPaymentIntent);
-paymentsRouter.post(
-    "/create-checkout-session",
-    createPayment.createCheckoutSession
-  );
+paymentsRouter.post("/create-checkout-session",createPayment.createCheckoutSession);
   paymentsRouter.post("/webhook", createPayment.handleWebhook);
-  paymentsRouter.get(
-    "/test-checkout-session",
-    createPayment.testCreateCheckoutSession
-  );
+  paymentsRouter.get( "/test-checkout-session",createPayment.testCreateCheckoutSession);
